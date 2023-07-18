2ee63d5431d4276df1be30d766954b1ff9a4f625
Arturo <sergiovargas199791@gmail.com>
2023-07-17 22:26:54 -0500

    Class Layer and layerCoefficients files were created

---
+++ Layer.py
@@ -0,0 +1,8 @@
+class Layer:
+    
+    def __init__(self, indice, modulus, poisson, thickness, pointsToAnalyze):
+        self.indice = indice
+        self.modulus = modulus
+        self.poisson = poisson
+        self.thickness = thickness
+        self.pointsToAnalyze = pointsToAnalyze
